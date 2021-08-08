4d3109acc949e22cc4be30dd74a49f04ec23d789
Zurarose <specter8471102@gmail.com>
2021-08-08 16:38:40 +0300

    heroku settings

--- Zhytlobud/urls.py
+++ Zhytlobud/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
    path("logout/", LogoutView.as_view(), name="logout"),
 
    path("subtabs/building_add/", views.building_add, name="building_add"),
-   path("subtabs/update_menu/", views.update_menu, name="update_menu"),
+   path("/update_menu/", views.update_menu, name="update_menu"),
    #For Ajax. Look at script in building_add.html.
    url(r'^ajax/getStreets/$', views.getStreets, name='getStreets'),
    url(r'^ajax/getSubways/$', views.getSubways, name='getSubways'),
