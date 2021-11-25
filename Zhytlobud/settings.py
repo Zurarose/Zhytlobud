c2cc118528b72a665d21c32fb5209371ed34458f
Олег Санников <specter8471102@gmail.com>
2021-11-25 12:26:47 +0200

    bug fixed

--- Zhytlobud/settings.py
+++ Zhytlobud/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'storages',
     #my app 
     'app',
 ]
@@ -113,7 +114,26 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_URL = '/static/'
+#AmazonS3
+USE_S3 = 'TRUE'
+
+if USE_S3 == 'TRUE':
+    # aws settings
+    AWS_ACCESS_KEY_ID = 'AKIAYBPL34RCTLWSYDSF'
+    AWS_SECRET_ACCESS_KEY = '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'
+    AWS_STORAGE_BUCKET_NAME = 'twenty-two'
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 static settings
+    AWS_LOCATION = 'static'
+    STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'TwentyTwo.storage_backends.MediaStorage'
+else:
+    STATIC_URL = 'app/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    MEDIA_URL = '/mediafiles/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 
 LOGOUT_REDIRECT_URL = "login"
